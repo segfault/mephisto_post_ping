cac27d2a33b1893ceba8faa7b6fa6d95a7c3f407
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2006-11-11 06:07:30 +0000

    threaded and error handled...
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_post_ping@4 c10dc6e2-ae21-0410-890e-c4c894b30763

--- lib/post_pinger.rb
+++ lib/post_pinger.rb
@@ -1,37 +1,46 @@
+require 'thread'
+
 class ArticlePingObserver < ActiveRecord::Observer
   observe Article
   SERVICES = [ "http://rpc.pingomatic.com/",
+#               "http://localhost:3000/",
 #               "http://rpc.technorati.com/rpc/ping",
 #               "http://ping.syndic8.com/xmlrpc.php",
   ]
 
   def after_save( article )
-    ping_all( article )
-  end
+    return unless article.published?
 
-  def after_update( article )
-    ping_all( article )
-  end 
+    SERVICES.each do |surl|
+      Thread.new(surl, article) do |url, art|
+        ping(url, art)
+      end
+    end     
+  end
 
 
   private
-  def ping_all(article)
-    return unless article.published?
-
-    SERVICES.each do |url|
-      ping(url, article)
-    end  
+  def logger
+    RAILS_DEFAULT_LOGGER
   end
 
   def ping(url, article)
     # see the weblogs ping spec @ http://www.weblogs.com/api.html
+    logger.info "sending weblog ping -> #{url}"
+
     cli = XMLRPC::Client.new2(url)
     title = article.site.title
     url = article.site.akismet_url
+
     feed_url = "%s/feed/atom.xml" % article.site.akismet_url
     tags = article.tags.join('|') # spec want's tags pipe delimeted
+
     res = cli.call2( 'weblogUpdates.extendedPing', title, url, feed_url, tags )
+    logger.info "ping result => '#{res}'"
     # not sure if we care about the result...?
+  rescue
+    logger.error "unable to send weblog ping -> #{url}"
+    # ignore ?
   end
                               
 end
