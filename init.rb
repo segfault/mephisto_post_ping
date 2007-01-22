b0fc2e49ada19ca068b2c1116c38fdfe31ce1c10
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2007-01-22 07:35:44 +0000

    works with the current trunk. extra shady requires of other plugins make life fun.
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_post_ping@75 c10dc6e2-ae21-0410-890e-c4c894b30763

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require 'vendor/plugins/permalink_fu/lib/permalink_fu'
+require 'vendor/plugins/permalink_fu/init'
 require 'post_pinger'
 require 'config'
 ActiveRecord::Base.observers << :article_ping_observer
